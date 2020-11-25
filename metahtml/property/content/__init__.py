651e831bc64e8f2f2fcc23189d6664c2371bdfa9
Mike Izbicki <mike@izbicki.me>
2020-11-25 01:32:38 -0800

    remove dead code

--- metahtml/property/content/__init__.py
+++ metahtml/property/content/__init__.py
@@ -1,15 +1,6 @@
 '''
 '''
 
-def extract(parser):
-
-    from metahtml.content.simple import simple_extractor
-
-    parser.meta['content'] = simple_extractor(parser)
-
-'''
-'''
-
 from metahtml.property.__common__ import BaseExtractor
 from metahtml.property.content.simple import simple_extractor
 
